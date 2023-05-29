3c04653f891c9f51a48f4c5a76e19446b1df9118
Cres <crescat@users.noreply.github.com>
2023-05-29 05:03:41 +0000

    delete old files

--- const.py
+++
@@ -1,7 +0,0 @@
-"""Constants for the xi_home integration."""
-
-DOMAIN = "xi_home"
-COMMAND_URL = "https://smartcareback.twinspace.co.kr:20001/device/command"
-DATA_URL = "https://smartcareback.twinspace.co.kr:20001/device/list-redis"
-STATUS_URL = "https://smartcareback.twinspace.co.kr:20001/device/status"
-PUBLIC_URL = "https://smartcareback.twinspace.co.kr:20001/public"
